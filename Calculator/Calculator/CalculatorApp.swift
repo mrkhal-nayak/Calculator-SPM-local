80806e0999b3bb364cd662c578487417278dbca7
Apple <apple@Apples-Laptop.local>
2024-12-18 16:33:02 +0530

    workflow folder is added

---
+++ Calculator/Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Apple on 18/12/24.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Calculator/CalculatorTests/CalculatorTests.swift
@@ -0,0 +1,16 @@
+//
+//  CalculatorTests.swift
+//  CalculatorTests
+//
+//  Created by Apple on 18/12/24.
+//
+
+import Testing
+
+struct CalculatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
