53684489c4cc7d26066cd9f67ca65fcf92b3f332
Shriyanshu Dwivedi <priyanksharma092001@gmail.com>
2024-12-19 10:25:11 +0530

    Update test-app.sh

--- .github/scripts/test-app.sh
+++ .github/scripts/test-app.sh
@@ -5,5 +5,5 @@ set -eo pipefail
 
 xcodebuild -workspace Calculator.xcworkspace \
             -scheme Calculator\ iOS \
-            -destination platform=iOS\ Simulator,OS=13.3,name=iPhone\ 11 \
+            -destination platform=iOS\ Simulator,OS=18.2,name=iPhone\ 16 \
             clean test | xcpretty
